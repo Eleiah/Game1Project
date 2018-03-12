7777b807296e98bf627078ad8ba6339164e4c727
L-1-4-M <37160299+L-1-4-M@users.noreply.github.com>
2018-03-12 13:29:00 -0700

    Add files via upload
    hey it's a button

---
+++ CPSC_1490_Final_Button.ino
@@ -0,0 +1,35 @@
+int button = 0;
+bool lastButtonState = 1;
+bool onOff = 0;
+
+void setup()
+{
+  pinMode(12, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+}
+
+bool checkButton() {
+  bool buttonState = digitalRead(12); // read from button
+  if (!lastButtonState && buttonState) { // if state changed from 0 to 1, set lastButtonState to buttonState and return 1
+    lastButtonState = buttonState;
+    return 1;
+  }
+  else { // else, set lastButtonState to buttonState and return zero
+    lastButtonState = buttonState;
+    return 0;
+  }
+}
+
+void loop() {
+  bool button = checkButton();
+  if (button) {
+    if (onOff)
+      onOff = 0;
+    else
+      onOff = 1;
+  }
+  if (onOff)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
+}

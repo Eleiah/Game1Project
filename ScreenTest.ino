1f1d3fa0b624ca6164f7c15a75f41468524d792c
TorinMStephens <37160126+TorinMStephens@users.noreply.github.com>
2018-03-25 15:22:35 -0700

    Update ScreenTest.ino

--- ScreenTest.ino
+++ ScreenTest.ino
@@ -11,6 +11,7 @@
 
 Adafruit_ILI9340 tft = Adafruit_ILI9340(_cs, _dc, _mosi, _sclk, _rst, _miso);
 
+//Character objects for Hero and Enemies
 class CHARACTER
 {
   public:
@@ -33,50 +34,59 @@ class CHARACTER
   }
 };
 
+//Spacing sets the size of each block, 16x16 pixels
 int spacing = 16;
 int wMap[3][3][15][15] = {};
-
-int inputPin = 22;
 bool dPad[4];
 int centre[2] = {7, 7};
 int origin = 3;
+//Initialize array or character objects
 CHARACTER Characters[20];
+//Boolean statments to make sure the button press is only registered once
+bool moveState = digitalRead(26);
+bool lastMoveState = digitalRead(26);
+bool attackState = digitalRead(27);
+bool lastAttackState = digitalRead(27);
 
 void setup() {
+  //Character 0 is the Hero
   Characters[0].properties(0,2,2,3,0,3,1);
+  //All other characters are enemies
   Characters[1].properties(1,13,13,0,0,3,1);
   Characters[2].properties(1,13,2,0,0,3,1);
   Characters[3].properties(1,2,13,0,0,3,1);
-  for(int i = 22;i<27;i++){
+  //Initialize pins for Dpad and buttons
+  for(int i = 22;i<28;i++){
     pinMode(i, INPUT);
     digitalWrite(i, HIGH);
   }
+  //Initialize the screen
   tft.begin();
+  //Set the screen to landscape and clear it
   tft.setRotation(3);
   tft.fillScreen(ILI9340_BLACK);
   initializeMap();
   drawMap();
 }
 
-bool moveState = digitalRead(26);
-bool lastMoveState = digitalRead(26);
-bool attackState = digitalRead(27);
-bool lastAttackState = digitalRead(27);
-
 void loop() {
+  //Check current state of buttons
   moveState = digitalRead(26);
   attackState = digitalRead(27);
+  //Check if user wants to change character direction
   faceDirection();
   //Inventory
+  //If the move button's state changes
   if(moveState && !lastMoveState) {
     heroMove();
     enemiesMove();
     lastMoveState = moveState;
   }
+  //If the button is in the same state, do nothing
   else{
     lastMoveState = moveState;
   }
-  
+  //Check if the attack button's state changes
   if(attackState && !lastAttackState){
     heroAttack();
     enemiesMove();
@@ -86,24 +96,47 @@ void loop() {
     lastAttackState = attackState;
   }
 }
-
+void drawMapMiddle(){
+  int poop[15][15]= {{1,1,1,1,1,1,0,1,1,1,1,1,1,1,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                    {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1}};
+}
+//Place the character after a map change
 void placeChar() {
-   switch(origin) {
+  //Remove Hero from current spot
+  deleteChunk(Characters[0].posX,Characters[0].posY);
+  wMap[0][0][Characters[0].posX][Characters[0].posY] = 0;
+  //Check where he should be placed
+   switch(Characters[0].cDirection) {
       case 0: Characters[0].posX = 14;
-         Characters[0].posX = 7;
+         Characters[0].posY = 7;
          break;
       case 1: Characters[0].posX = 7;
-         Characters[0].posY = 1;
+         Characters[0].posY = 14;
          break;
-      case 2: Characters[0].posX = 1;
+      case 2: Characters[0].posX = 0;
          Characters[0].posY = 7;
          break;
       case 3: Characters[0].posX = 7;
-         Characters[0].posY = 14;
+         Characters[0].posY = 0;
          break;
    }
+   //Place the Hero on the map
    wMap[0][0][Characters[0].posX][Characters[0].posY] = 2;
-   drawHero(Characters[0].posX,Characters[0].posY,Characters[0].cDirection);
+   //Draw the hero
+   drawHero(Characters[0].posX,Characters[0].posY);
 }
 
 void heroMove() {
@@ -126,7 +159,10 @@ void heroMove() {
       deleteChunk(Characters[0].posX,Characters[0].posY);
       Characters[0].posX = Characters[0].posX + newPos[0];
       Characters[0].posY = Characters[0].posY + newPos[1];
-      drawHero(Characters[0].posX,Characters[0].posY,Characters[0].cDirection);
+      drawHero(Characters[0].posX,Characters[0].posY);
+  }
+  if(Characters[0].posX <= 0 || Characters[0].posX >= 14 || Characters[0].posY <= 0 || Characters[0].posY >= 14){
+    placeChar();
   }
 }
 
@@ -223,9 +259,10 @@ void faceDirection() {
   for(int i = 0;i<4;i++) {
     dPad[i] = digitalRead(22+i);
     if(!dPad[i] && Characters[0].cDirection !=i) {
-      deleteChunk(Characters[0].posX,Characters[0].posY);
-      drawHero(Characters[0].posX,Characters[0].posY,i);
       Characters[0].cDirection = i;
+      deleteChunk(Characters[0].posX,Characters[0].posY);
+      drawHero(Characters[0].posX,Characters[0].posY);
+      
     }
   }
 }
@@ -243,6 +280,10 @@ void initializeMap() {
     wMap[0][0][i][0] = 1;
     wMap[0][0][i][14] = 1;
   }
+  wMap[0][0][0][7] = 0;
+  wMap[0][0][14][7] = 0;
+  wMap[0][0][7][0] = 0;
+  wMap[0][0][7][14] = 0;
   for(int i = 5; i < 10; i++) {
       wMap[0][0][i][7] = 1;
       wMap[0][0][7][i] = 1;
@@ -267,26 +308,69 @@ void drawMap() {
 
 void drawTile(int xPos,int yPos,int tileType) {
   if(tileType == 1) {
-    tft.fillRect(xPos,yPos,spacing,spacing,ILI9340_WHITE);
+    tft.fillRect(xPos,yPos,spacing,spacing,0x8430);//Darker
+    tft.fillRect(xPos+1,yPos+1,4,4,0xAD55);//Lighter
+    tft.fillRect(xPos+7,yPos+1,3,3,0xAD55);
+    tft.fillRect(xPos+12,yPos+1,3,3,0xAD55);
+    tft.fillRect(xPos+1,yPos+7,6,3,0xAD55);
+    tft.fillRect(xPos+9,yPos+6,6,3,0xAD55);
+    tft.fillRect(xPos+1,yPos+12,8,3,0xAD55);
+    tft.fillRect(xPos+11,yPos+11,4,4,0xAD55);
   }
   if(tileType == 4) {
     //draw a door
   }
+  //Lava
+  if(tileType == 8){
+    //0x6B6D Darkest
+    //0xFD42 Dark
+    //0xFB40 Light
+    tft.fillRect(xPos,yPos,16,16,0x6B6D);
+    tft.fillRect(xPos,yPos,2,4,0xFD42);
+    tft.fillRect(xPos+4,yPos,8,2,0xFD42);
+    tft.fillRect(xPos+6,yPos+2,4,2,0xFD42);
+    tft.fillRect(xPos+2,yPos+4,4,8,0xFD42);
+    tft.fillRect(xPos,yPos+6,8,4,0xFD42);
+    tft.fillRect(xPos+10,yPos+4,4,4,0xFD42);
+    tft.fillRect(xPos+14,yPos,2,4,0xFD42);
+    tft.fillRect(xPos+12,yPos+10,4,4,0xFD42);
+    tft.fillRect(xPos+6,yPos+12,4,2,0xFD42);
+    tft.fillRect(xPos+4,yPos+14,8,2,0xFD42);
+  }
+  //Wood Planks
+  if(tileType == 9){
+    tft.fillRect(xPos+1,yPos+1,2,15,0xD460);
+    tft.fillRect(xPos+5,yPos,2,16,0xD460);
+    tft.fillRect(xPos+9,yPos,2,16,0xD460);
+    tft.fillRect(xPos+13,yPos,2,16,0xD460);
+    tft.drawFastVLine(xPos,yPos,16,0x9320);
+    tft.drawFastVLine(xPos+15,yPos,16,0x9320);
+    tft.fillRect(xPos+3,yPos,2,16,0x9320);
+    tft.fillRect(xPos+7,yPos,2,16,0x9320);
+    tft.fillRect(xPos+11,yPos,2,16,0x9320);
+    tft.drawFastHLine(xPos+1,yPos,2,0x9320);
+    tft.drawFastHLine(xPos+1,yPos+9,2,0x9320);
+    tft.drawFastHLine(xPos+5,yPos+4,2,0x9320);
+    tft.drawFastHLine(xPos+5,yPos+13,2,0x9320);
+    tft.drawFastHLine(xPos+9,yPos+7,2,0x9320);
+    tft.drawFastHLine(xPos+9,yPos+15,2,0x9320);
+    tft.drawFastHLine(xPos+13,yPos+2,2,0x9320);
+    tft.drawFastHLine(xPos+13,yPos+12,2,0x9320);
+    }
 }
 
 void drawEnemy(int i){
   tft.fillRect(Characters[i].posX*spacing,Characters[i].posY*spacing,16,16,ILI9340_MAGENTA);
 }
 
-void drawHero(int x,int y,int heroDirection) {
+void drawHero(int x,int y) {
   //Colours, BLACK,BLUE,RED,GREEN,CYAN,MAGENTA,YELLOW,WHITE
   //tft.drawFastVLine(x,y,length, colour)
   //tft.drawFastHLine(x,y,length, colour)
   //tft.drawPixel(x*10,y*10,ILI9340_BLACK)
   //tft.drawRect(x,y,width,height,colour)
-  heroDirection%=4;
   //Hero facing left
-  if(heroDirection == 0) {
+  if(Characters[0].cDirection == 0) {
     //Draw Hat
     tft.drawFastHLine(x*spacing+5,y*spacing,11,0x1CB0);
     tft.drawFastHLine(x*spacing+4,y*spacing+1,11,0x1CB0);
@@ -321,7 +405,7 @@ void drawHero(int x,int y,int heroDirection) {
     tft.drawFastHLine(x*spacing+11,y*spacing+15,3,0xA3C0);
   //Hero facing up  
   }
-  else if(heroDirection == 1) {
+  else if(Characters[0].cDirection == 1) {
     tft.drawFastHLine(x*spacing+5,y*spacing,6,0x1CB0);
     tft.drawFastHLine(x*spacing+4,y*spacing+1,8,0x1CB0);
     tft.fillRect(x*spacing+3,y*spacing+2,10,4,0x1CB0);
@@ -350,7 +434,7 @@ void drawHero(int x,int y,int heroDirection) {
     
   //Hero facing right  
   }
-  else if(heroDirection == 2) {
+  else if(Characters[0].cDirection == 2) {
     //Draw Hat
     tft.drawFastHLine(x*spacing,y*spacing,11,0x1CB0);
     tft.drawFastHLine(x*spacing+1,y*spacing+1,11,0x1CB0);
